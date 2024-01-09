104b91f8c8f7ac3ebf017c2499e837591b9a851b
nirnanaaa <flo.fk44@gmail.com>
2024-01-09 07:19:55 +0100

    fix key facts

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,7 +14,7 @@ export default function About() {
 
             <SimpleLayout
                 title="Scaleable Computing Infrastructure and Developer-Focused Computing Experiences."
-                intro={'I am a Cloud-Native Developer and Dev-Ops Engineer. Freelancing for over 6 years now, I’ve been actively contributing to improve my clients position in their industries. With my expertise not only in software development, but also in the technologies and techniques at an infrastructure and architectural level, I can help to make pragmatic decisions that will deliver measurable results. Software engineering and architecting for me, started out as just a hobby, but quickly evolved into a passion, that lasts until today. I constantly seek for new adventures and challenges that will actually be of value for my customers.'}
+                intro={'I am a Cloud-Native Developer and Dev-Ops Engineer. Freelancing for over 9 years now, I’ve been actively contributing to improve my clients position in their industries. With my expertise not only in software development, but also in the technologies and techniques at an infrastructure and architectural level, I can help to make pragmatic decisions that will deliver measurable results. Software engineering and architecting for me, started out as just a hobby, but quickly evolved into a passion, that lasts until today. I constantly seek for new adventures and challenges that will actually be of value for my customers.'}
             >
             
             If you need any help decluttering your own infrastructure, please contact me at <a href="mailto:info@florian-kasper.com" className="link">info@florian-kasper.com</a>.
